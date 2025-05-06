39bb741bd8bd44e41a861f48e2c3939c72160d43
André Lashley <andre.lashley@gmail.com>
2025-05-05 19:56:22 -0700

    Finish app shell

---
+++ src/components/UserComponent.vue
@@ -0,0 +1,89 @@
+<template>
+    <div id="userComponent">
+        <img
+            @click="openMenu = !openMenu"
+            data-tooltip-target="tooltip-no-arrow-user"
+            data-tooltip-placement="bottom"
+            src="https://placehold.co/40"
+            class="rounded-full w-8 cursor-pointer">
+
+            <div 
+                v-show="!openMenu"
+                id="tooltip-no-arrow-user"
+                role="tooltip"
+                class="
+                    inline-block 
+                    absolute 
+                    invisible 
+                    text-xs 
+                    z-10 
+                    py-1 
+                    px-2 
+                    font-medium 
+                    text-white 
+                    rounded-sm 
+                    shadow-sm 
+                    opacity-0 
+                    tooltip 
+                    bg-gray-600
+                    delay-150
+                "
+                >
+                <div>Google Account</div>
+                <div class="text-gray-300">
+                    John Doe
+                </div>
+                <div class="text-gray-300">
+                    john.doe@gmail.com
+                </div>
+            </div>
+
+            <div 
+                v-show="openMenu"
+                class="
+                    absolute 
+                    z-10
+                    w-80
+                    right-2
+                    bg-white
+                    top-14
+                    rounded-lg
+                    "
+                >
+
+                <div class="w-full flex justify-center">
+                    <img
+                    src="https://placehold.co/70"
+                    class="rounded-full w-20 mt-4" />
+                </div>
+                <div class="text-gray-700 w-full flex justify-center mt-2 text-lg">
+                    John Doe
+                </div>
+                <div class="text-gray-700 w-full flex justify-center mt-2 text-sm pb-4 border-b">
+                    john.doe@mail.com
+                </div>
+                <div class="flex justify-center my-5">
+                    <button class="
+                        bg-transparent
+                        text-xs
+                        hover:bg-gray-100
+                        text-gray-600
+                        font-semibold
+                        py-2
+                        px-4
+                        border
+                        border-gray-300
+                        rounded
+                        "   >
+                        Sign out of Gmail
+                    </button>
+                </div>
+            </div>
+    </div>
+</template>
+
+<script setup>
+import {ref} from 'vue';
+
+let openMenu = ref(false);
+</script>
---
+++ src/views/template/EmailView.vue
@@ -0,0 +1,228 @@
+<template>
+    <div id="EmailView" class="bg-gray-50 h-screen">
+        <div class="flex items-center p-2">
+            <div class="logo-section flex justify-start items-center px-3.5">
+                <IconComponent
+                    iconString="menu"
+                    iconColor="#636363"
+                    :iconSize="19"
+                    hoverColor="hover:bg-gray-200"
+                    text="Main menu"
+                    class="-ml-2 mr-2"
+                />
+                <img width="107" src="../../../public/img/GmailLogo.png" />
+            </div>
+            <div class="flex w-full justify-between">
+                <div class="input-width w-full bg-gray-200 flex items-center p-1 px-2 rounded-lg">
+                    <IconComponent
+                        iconString="magnify"
+                        iconColor="#636363"
+                        :iconSize="19"
+                        hoverColor="hover:bg-gray-200"
+                        text="Search"
+                    />
+                    <input
+                        placeholder="Search mail"
+                        class="
+                            w-full
+                            h-10
+                            bg-gray-200
+                            border-transparent
+                            border-none
+                            focus:ring-0
+                            outline-none
+                            placeholder-gray-900
+                        "
+                        type="text"
+                        >
+                        <IconComponent
+                            iconString="tune"
+                            iconColor="#636363"
+                            :iconSize="19"
+                            hoverColor="hover:bg-gray-200"
+                            text="Show search options"
+                        />
+                </div>
+                <div class="flex w-32 justify-between items-center ml-6">
+                    <IconComponent
+                        iconString="cog"
+                        iconColor="#636363"
+                        :iconSize="19"
+                        hoverColor="hover:bg-gray-200"
+                        text="Settings"
+                    />
+
+                    <IconComponent
+                        iconString="apps"
+                        iconColor="#636363"
+                        :iconSize="19"
+                        hoverColor="hover:bg-gray-200"
+                        text="Google apps"
+                    />
+
+                    <UserComponent />
+                </div>
+            </div>
+        </div>
+
+        <div class="flex w-full justify-between">
+            <div id="SideMenu" class="side-menu">
+                <div class="
+                    flex
+                    items-center
+                    justify-center
+                    bg-sky-200
+                    w-36
+                    h-8
+                    mt-2
+                    rounded-2xl
+                    ml-2
+                    p-7
+                    cursor-pointer
+                    "
+                >
+                    <PencilOutlineIcon :size="25" class="mr-4" />
+                    <span class="text-sm">compose</span>
+                </div>
+
+                <div class="my-5"></div>
+
+                <div 
+                    class="
+                        side-menu-item
+                        flex
+                        justify-between
+                        px-6
+                        py-1.5
+                        bg-blue-100
+                        rounded-r-full
+                    ">
+
+                    <div class="flex items-center">
+                        <InboxIcon :size="17" />
+                        <div class="text-sm pl-4 font-semibold">
+                            Inbox
+                        </div>
+                    </div>
+
+                    <div class="text-xs font-semibold">
+                        26
+                    </div>
+
+                </div>
+
+                <div class="flex justify-between px-6 py-1.5">
+                    <div class="flex items-center">
+                        <StarOutlineIcon :size="17" />
+                        <div class="text-sm pl-4">Starred</div>
+                    </div>
+                </div>
+
+                <div class="flex justify-between px-6 py-1.5">
+                    <div class="flex items-center">
+                        <ClockOutlineIcon :size="17" />
+                        <div class="text-sm pl-4">Snoozed</div>
+                    </div>
+                </div>
+
+                <div class="flex justify-between px-6 py-1.5">
+                    <div class="flex items-center">
+                        <SendOutlineIcon :size="17" />
+                        <div class="text-sm pl-4">Sent</div>
+                    </div>
+                </div>
+
+                <div class="flex justify-between px-6 py-1.5">
+                    <div class="flex items-center">
+                        <FileOutlineIcon :size="17" />
+                        <div class="text-sm pl-4">Drafts</div>
+                    </div>
+                </div>
+            </div>
+
+            <router-view />
+
+            <div class="m-4">
+                <div class="w-6 h-6 flex justify-center mb-7">
+                    <img src="img/GoogleCalendar.png" alt="" class="object-center">
+                </div>
+
+                <div class="w-6 h-6 flex justify-center mb-7">
+                    <img src="img/GoogleKeep.png" alt="" class="object-center">
+                </div>
+
+                <div class="w-6 h-6 flex justify-center mb-7">
+                    <img src="img/GoogleTasks.png" alt="" class="object-center">
+                </div>
+
+                <div class="w-6 h-6 flex justify-center mb-7">
+                    <img src="img/GoogleContacts.png" alt="" class="object-center">
+                </div>
+
+                <div class="w-6 flex mb-7 border border-gray-300"></div>
+
+                <div class="w-6 h-6 flex justify-center mb-7">
+                    <PlusIcon />
+                </div>
+            </div>
+        </div>
+
+        <div 
+            class="
+                absolute
+                bottom-0
+                right-0
+                mr-20
+                rounded-t-lg
+                shadow-2xl
+                bg-white
+            ">
+            <div class="
+                flex
+                items-center
+                justify-between
+                rounded-t-lg
+                w-full
+                px-3.5
+                py-2.5
+                bg-gray-200
+                ">
+
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<script setup>
+import IconComponent from '@/components/IconComponent.vue';
+import UserComponent from '@/components/UserComponent.vue';
+import PencilOutlineIcon from "vue-material-design-icons/PencilOutline.vue";
+import InboxIcon from "vue-material-design-icons/Inbox.vue";
+import StarOutlineIcon from "vue-material-design-icons/StarOutline.vue";
+import ClockOutlineIcon from "vue-material-design-icons/ClockOutline.vue";
+import SendOutlineIcon from "vue-material-design-icons/SendOutline.vue";
+import FileOutlineIcon from "vue-material-design-icons/FileOutline.vue";
+import PlusIcon from "vue-material-design-icons/Plus.vue";
+
+</script>
+
+<style lang="scss">
+    #EmailView {
+        .logo-section {
+            min-width: 250px;
+        }
+
+        .input-width {
+            max-width: 700px;
+        }
+
+        .side-menu-item {
+            width: 250px;
+        }
+
+        .side-menu {
+            width: 300px;
+        }
+    }
+</style>
